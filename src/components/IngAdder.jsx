32e26b6c62d007909b0856653c6aa7cb54681b7e
clawbuster <pereiraarjun@gmail.com>
2025-11-06 18:57:29 +0530

    Fixed adding issue, recipe showing toggle

--- src/components/IngAdder.jsx
+++ src/components/IngAdder.jsx
@@ -4,14 +4,14 @@ import './IngAdder.css';
 
 export default function IngAdder(){
     const [IngredientList, setIngredientList] = useState([]);
-    const ing = '';
+    const [showRecipe, setShowRecipe] = useState(false);
 
     function addIng(e){
         e.preventDefault();
 
         const curIng = new FormData(e.currentTarget);
         setIngredientList((item)=>[...item, curIng.get('ing')]);
-        curIng.set('ing', '');
+        e.currentTarget.reset();
     }
 
     function removeIng(index){
@@ -23,6 +23,10 @@ export default function IngAdder(){
         return <li key={index}>{item} <button onClick={() => removeIng(index)}>X</button></li>
     })
 
+    function genRecipe(){
+        setShowRecipe((prev)=>prev=!prev);
+    }
+
     return(
         <>
         <form onSubmit={addIng}>
@@ -32,6 +36,21 @@ export default function IngAdder(){
         <ul>
             {newList}
         </ul>
+        {IngredientList.length > 3 &&
+            <section>
+                <h3>Ready to create a recipe?</h3>
+                <button onClick={genRecipe}>{showRecipe? "Hide" : "Generate" } Recipe</button>
+            </section>
+        }
+        {showRecipe && 
+            <div className="recipe-info">
+                <h4>
+                    Recipe info
+                </h4>
+                <p>jajaja</p>
+            </div>
+        }
+        
         </>
     )
 
